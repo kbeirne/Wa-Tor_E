717e11c339e9a33e275060133e59156dc450dbf6
Kevin Beirne <kevin@Klauss.(none)>
2012-10-18 16:12:14 +0100

    Got world printing with fish randomly placed on grid

--- Fish.h
+++ Fish.h
@@ -2,33 +2,88 @@
 #define FISH_H
 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+//#include "World.h"
+
+int Fish_Breed = 0;
+
+typedef enum { WEST, EAST, SOUTH, NORTH } Direction;
 
 struct Fish
 {
 	char * m_name;
+	int m_alive;
+	int m_timeAlive;
 	float m_posX;
 	float m_posY;
 };
 
 // prototypes
-void Fish_Setup(struct Fish * thisObj);
-void Fish_Draw(struct Fish * thisObj);
-void Fish_Update(struct Fish * thisObj);
+void Fish_Setup(struct Fish * f);
+void Fish_Draw(struct Fish * f);
+void Fish_Update(struct Fish * f);
 
-void Fish_Setup(struct Fish * thisObj)
+void Fish_Setup(struct Fish * f)
 {
-	thisObj->m_name = "Mr.Fish";
-	thisObj->m_posX = 0;
-	thisObj->m_posY = 0;
+	srand(time(NULL));
+	f->m_name = "Mr.Fish";
+	f->m_posX = 0;
+	f->m_posY = 0;
+	f->m_alive = 0;
+	f->m_timeAlive = 0;
 }
 
-void Fish_Draw(struct Fish * thisObj)
+void Fish_Draw(struct Fish * f)
 {
-	printf("%.7s\n", thisObj->m_name);
+	printf("%.7s\n", f->m_name);
 }
 
-void Fish_Update(struct Fish * thisObj)
+void Fish_Update(struct Fish * f)
 {
+	f->m_timeAlive++;
+	Direction newLocation = rand() % 4;//0-3
+	switch(newLocation)
+	{
+		case WEST:
+		{
+			if(f->m_posX < 0)
+			{
+				f->m_posX = (GRID_WIDTH - 1);
+			}
+			else
+			{
+				f->m_posX--;
+			}
+			break;
+		}
+		case EAST:
+		{
+			f->m_posX++;
+			if(f->m_posX >= (GRID_WIDTH))
+				f->m_posX = 0;
+			break;
+		}
+		case NORTH:
+		{
+			if(f->m_posY < 0)
+				f->m_posX = (GRID_HEIGHT - 1);
+			else 
+				f->m_posX--;
+			break;
+		}
+		case SOUTH:
+		{
+			if(f->m_posY > GRID_HEIGHT)
+			{
+			}
+			else
+			{
+				f->m_posX++;
+			}
+			break;
+		}
+	};
 }
 
 #endif
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
-
 #include <stdio.h>
+#include "Globals.h"
 #include "Time.h"
+#include "World.h"
 #include "Fish.h"
 
 //prototypes
@@ -8,23 +9,26 @@ void GameLoop();
 
 struct Time myTime;
 struct Fish myFish;
+struct World world;
+
 int main()
 {
+	GRID_WIDTH = 75;
+	GRID_HEIGHT = 20;
 	// setup 
 	Time_Setup(&myTime);
 	Fish_Setup(&myFish);
-
-	printf("Start!!!\n");
-
+	World_Setup(&world, 30);
+	World_Draw(&world);
+	
 	//GameLoop();
 
-	
 	return (0);
 }
 
 void GameLoop()
 {	
-	while(1 == 1)
+	while(1)
 	{
 		Time_Update(&myTime);
 		Time_Wait(&myTime, 1);
