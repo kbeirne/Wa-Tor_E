82cf22c1cebb7dc8c1cfd7bae755d65119892ee3
declanfinnerty <declan.finnerty@gmail.com>
2012-10-24 16:49:00 +0100

    fixed shark problems

--- Creature.h
+++ Creature.h
@@ -9,7 +9,7 @@ int BreedCount = 3;//NOTE: more than 1 doesn't work ???
 
 typedef enum { NORTH, EAST, SOUTH, WEST } Direction;
 typedef enum { FISH, SHARK } Species;
-int timeToStarve = 8;
+int timeToStarve = 4;// problem, seems to act as though the velue is half what its given
 
 struct Position
 {
@@ -22,7 +22,7 @@ struct Creature
 	int m_alive;
 	int m_timeAlive;
 	int m_timesMoved;
-	int m_starve; // the numder of chronons before the shark starves
+	int m_health; // the numder of chronons before the shark starves
 	Species m_type;
 	struct Position m_pos;
 };
@@ -41,22 +41,28 @@ void Creature_Setup(struct Creature * c)
 	c->m_alive = 0;
 	c->m_timeAlive = 0;
 	c->m_timesMoved = 0;
-	c->m_starve = timeToStarve;
+	c->m_health = timeToStarve;
 	c->m_type = FISH;
 }
 
 //Don't need to pass in c if you have grid and posToUpdate but more efficient then constantly dereferencing posToUpdate on grid
 void Creature_Update(struct Creature * c, struct Creature *** grid, struct Position * posToUpdate)
 {
-	c->m_starve--;
+
+if( c->m_alive != 0 )
+{
+
 	c->m_timeAlive++;
 	Direction startLocation = rand() % 4;//0-3
 
 	// Shark Starve
 	if(c->m_type == SHARK)
-	{
-		if(c->m_starve <= 0)
+	{	
+		c->m_health--;
+		if(c->m_health <= 0)
+		{
 			c->m_alive = 0;
+		}
 	}
 
 	if(startLocation == EAST) goto east;
@@ -175,7 +181,10 @@ void Creature_Update(struct Creature * c, struct Creature *** grid, struct Posit
 
 	move:
 	{
-		Creature_Make_Move(c, grid, posToUpdate);
+		if(c->m_alive != 0)
+		{
+			Creature_Make_Move(c, grid, posToUpdate);
+		}
 		goto end;
 	}
 
@@ -190,6 +199,9 @@ void Creature_Update(struct Creature * c, struct Creature *** grid, struct Posit
 	end:
 	{
 	}
+
+}
+
 }
 
 //NOTE: Make_Move encompasses all fish actions (moving and breeding) not just moving, Shark_Make_Move should be the same
@@ -207,6 +219,7 @@ void Creature_Make_Move(struct Creature * c, struct Creature *** grid, struct Po
 		if((c->m_timeAlive % BreedCount) == 0)//If bred then set times moved to 0, don't want to copy old life span
 		{
 			(*grid)[newPos->m_x][newPos->m_y].m_timesMoved = 0;
+			(*grid)[newPos->m_x][newPos->m_y].m_health = timeToStarve;
 			//printf("Breeding");
 		}
 		else //otherwise kill old fish (i.e move him)
@@ -233,7 +246,7 @@ void Creature_Copy(struct Creature * c, struct Creature *** grid, struct Positio
 
 	(*grid)[newPos->m_x][newPos->m_y].m_type = (*grid)[c->m_pos.m_x][c->m_pos.m_y].m_type;
 
-	(*grid)[newPos->m_x][newPos->m_y].m_starve = (*grid)[c->m_pos.m_x][c->m_pos.m_y].m_starve;
+	(*grid)[newPos->m_x][newPos->m_y].m_health = (*grid)[c->m_pos.m_x][c->m_pos.m_y].m_health;
 
 	(*grid)[newPos->m_x][newPos->m_y].m_pos = *newPos;
 	
@@ -246,6 +259,12 @@ void Shark_Eat_Fish(struct Creature * cA, struct Creature * cB, struct Creature 
 {
 	struct Creature * shark;
 	struct Creature * fish;
+
+	if(cA->m_type == SHARK && cB->m_type == SHARK)// which is shark
+	{
+		exit(0);
+	}
+
 	if(cA->m_type == SHARK)// which is shark
 	{
 		shark = cA;
@@ -259,10 +278,10 @@ void Shark_Eat_Fish(struct Creature * cA, struct Creature * cB, struct Creature 
 
 	int oldPosX = shark->m_pos.m_x;
 	int oldPosY = shark->m_pos.m_y;
+	shark->m_health = timeToStarve;
 	Creature_Copy(shark, grid, &fish->m_pos);//Copy to new location
 
 	(*grid)[oldPosX][oldPosX].m_alive = 0;// kill old shark
-	shark->m_starve = timeToStarve;
 
 	fish->m_alive = 0; 	// kill fish
 
